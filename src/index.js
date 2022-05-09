c31309d023f1bfbc21edb38ffdf8a4b20e4cdb9d
GregNicholas <59461870+GregNicholas@users.noreply.github.com>
2022-05-09 12:09:41 -0700

    draw new cards, check status, display cards

--- src/index.js
+++ src/index.js
@@ -5,27 +5,34 @@ const startGameBtn = document.getElementById("start-game");
 const newCardBtn = document.getElementById("new-card");
 const card1 = document.querySelector(".card1");
 const card2 = document.querySelector(".card2");
+const cardsDisplay = document.getElementById("cards-display");
 let deckId = null;
 let cards = [];
 let sum = 0;
 let hasBlackJack = false;
 let isAlive = false;
 let message = "";
-
+console.log("HASBJ: ", hasBlackJack);
 const getDeck = async () => {
+  cardsDisplay.innerHTML = "";
   fetch("https://deckofcardsapi.com/api/deck/new/draw/?count=2")
     .then((res) => res.json())
     .then((data) => {
       deckId = data.deck_id;
       cards = data.cards;
+      const card1 = document.createElement("div");
+      card1.classList.add("card-slot", "card1");
+      const card2 = document.createElement("div");
+      card2.classList.add("card-slot", "card2");
       card1.innerHTML = `
         <img src=${cards[0].images.png} class="card" />
       `;
       card2.innerHTML = `
         <img src=${cards[1].images.png} class="card" />
       `;
+      cardsDisplay.appendChild(card1);
+      cardsDisplay.appendChild(card2);
       sum = getSum(cards);
-      console.log(sum);
       sumEl.textContent = sum;
       if (sum === 21) {
         hasBlackJack = true;
@@ -55,10 +62,10 @@ const getSum = (arr) => {
 
   let numAces = arr.filter((c) => c.value === "ACE").length;
   console.log("ACES: ", numAces);
-  let sum = arr.reduce((a, b) => {
-    console.log(values[a.value]);
-    return values[a.value] + values[b.value];
-  });
+
+  let sum = arr.reduce((acc, cur) => {
+    return acc + values[cur.value];
+  }, 0);
   if (sum > 21) {
     while (numAces > 0 && sum > 21) {
       sum -= 10;
@@ -70,6 +77,7 @@ const getSum = (arr) => {
 
 function startGame() {
   getDeck();
+  hasBlackJack = false;
   isAlive = true;
 }
 
@@ -84,11 +92,26 @@ const drawNewCard = () => {
       .then((res) => res.json())
       .then((data) => {
         newCard = data.cards[0];
-        console.log(newCard);
+        cards.push(newCard);
+        console.log(cards);
+        const newCardSlot = document.createElement("div");
+        newCardSlot.classList.add("card-slot", "card3");
+        newCardSlot.innerHTML = `<img src=${newCard.images.png} class="card" />`;
+        cardsDisplay.appendChild(newCardSlot);
+        sum = getSum(cards);
+        console.log(sum);
+        sumEl.textContent = sum;
+        if (sum === 21) {
+          hasBlackJack = true;
+        }
+        if (sum > 21) {
+          isAlive = false;
+        }
       })
       .catch((err) => {
         console.log(err);
       });
+
     // sum = getSum(cards);
     // sumEl.textContent = sum;
     // if (sum === 21) {
